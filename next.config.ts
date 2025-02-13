fa969df15a2623fa0eb3441b04cca35cb8111cca
Endra Dwi <endradwijamludin@gmail.com>
2025-02-14 04:32:04 +0700

    coba

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
+  
 };
 
 export default nextConfig;
